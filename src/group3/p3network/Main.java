78c91103b3a4156fe5d8863db75538abb489aab5
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-05 15:36:13 +0800

    Can now list the filenames of the videos

--- src/group3/p3network/Main.java
+++ src/group3/p3network/Main.java
@@ -2,7 +2,6 @@ package group3.p3network;
 
 public class Main {
     public static void main(String[] args){
-        Directory dir = new Directory();
-        System.out.println(dir.getCurrentDirectory());
+        new VideoSearch().gatherVideos(new Directory());
     }
 }
--- src/group3/p3network/VideoSearch.java
+++ src/group3/p3network/VideoSearch.java
@@ -1,8 +1,20 @@
 package group3.p3network;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class VideoSearch {
+    public ArrayList<Video> gatherVideos(Directory path) {
+        ArrayList<Video> videos = new ArrayList<>();
 
-    public VideoSearch() {
+        ArrayList<Path> gatheredVideoFiles = new ArrayList<>();
 
+        for (String result : new File(".").list()) {
+            System.out.println(result);
+        }
+
+
+        return videos;
     }
 }
