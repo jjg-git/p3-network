7fb3b88d3a87c3d9b96e97d2c36b17f0ccf657a8
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-05 16:15:34 +0800

    In CheckDirectory, let Files.isDirectory do the checking

--- src/group3/p3network/CheckDirectory.java
+++ src/group3/p3network/CheckDirectory.java
@@ -2,17 +2,25 @@ package group3.p3network;
 
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 
 public class CheckDirectory {
     public boolean check(String path) {
         // Check if it's a valid format directory
         // Source: https://regex101.com/r/xCywRh/1
 
-        if (!(path.matches(".+((?=\\\\)|(?=\\/)/?)"))) {
-            return false;
-        }
+        // if (!(path.matches(".+((?=\\\\)|(?=\\/)/?)"))) {
+        //     return false;
+        // }
 
-        if (!Files.isDirectory(FileSystems.getDefault().getPath(path))) {
+        try {
+            if (!Files.isDirectory(FileSystems.getDefault().getPath(path))) {
+                System.out.println("Path is not a directory!");
+                return false;
+            }
+        }
+        catch (InvalidPathException e) {
+            System.out.println("Invalid path format!");
             return false;
         }
 
