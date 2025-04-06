7f00f27ca0aa31fcbc56c15b2f838c460c43729e
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 10:59:56 +0800

    Added toString in Video.java

--- src/group3/p3network/Video.java
+++ src/group3/p3network/Video.java
@@ -1,4 +1,11 @@
 package group3.p3network;
 
 public class Video {
+    @Override
+    public String toString() {
+        return "Video{" +
+            "videoFile=" + videoFile +
+            ", size=" + size +
+            '}';
+    }
 }
