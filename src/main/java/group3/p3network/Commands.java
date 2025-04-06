cb96535a146a5dc51f90396dfcf65a8a23403519
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 22:43:45 +0800

    Added a class Commands that contains a collection of static Command.

---
+++ src/main/java/group3/p3network/Commands.java
@@ -0,0 +1,7 @@
+package group3.p3network;
+
+public class Commands {
+    private static Command.Builder builder = Command.newBuilder();
+
+    public static Command ListVideos = builder.setCommand("List Videos").build();
+}
