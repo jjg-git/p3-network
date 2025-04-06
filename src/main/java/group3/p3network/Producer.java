2c652cf83bd55107f07793110c85be2506a056d6
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 16:59:07 +0800

    Built a gRPC server in the Producer.java

--- src/main/java/group3/p3network/Producer.java
+++ src/main/java/group3/p3network/Producer.java
@@ -1,19 +1,30 @@
 package group3.p3network;
 
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
 import io.grpc.stub.StreamObserver;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Producer {
-    private final Server server;
+    private Server server;
 
-    private void start(int producerThreads) {
+    private void start(int producerThreads) throws IOException {
         final int port = 50051;
 
         ExecutorService executor =
             Executors.newFixedThreadPool(producerThreads);
+        server = Grpc.newServerBuilderForPort(
+            port,
+            InsecureServerCredentials.create()
+        )
+            .executor(executor)
+            .addService(new SendingVideoService())
+            .build()
+            .start();
     }
 
     public static void main(String[] args){
