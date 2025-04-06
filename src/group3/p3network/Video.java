81e6093fda66b8894012600e8a28628319a36471
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 11:02:04 +0800

    Added constructor of Video.java that reads the file and set the fields automatically

--- src/group3/p3network/Video.java
+++ src/group3/p3network/Video.java
@@ -1,6 +1,22 @@
 package group3.p3network;
 
+import java.io.*;
+
 public class Video {
+    File videoFile;
+    long size;
+    String filename;
+    Directory directory;
+    public Video(String filename) {
+        this.videoFile = new File(filename);
+        try {
+            this.size = this.videoFile.length();
+        } catch (SecurityException e) {
+            System.err.println(e.getMessage());
+        }
+        this.filename = this.videoFile.getName();
+        this.directory = new Directory();
+    }
     @Override
     public String toString() {
         return "Video{" +
