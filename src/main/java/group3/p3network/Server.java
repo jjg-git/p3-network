b48c76718060b5be2640a00bfad8b06626da1072
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 16:40:21 +0800

    Added sendVideo in Server.java

--- src/main/java/group3/p3network/Server.java
+++ src/main/java/group3/p3network/Server.java
@@ -1,7 +1,22 @@
 package group3.p3network;
 
+import com.google.protobuf.ByteString;
+import io.grpc.stub.StreamObserver;
+
 public class Server {
     public static void main(String[] args){
         new VideoSearch(new Directory("server-dir/")).gatherVideos();
+    }
+
+    private static class SendingVideoService
+        extends SendingVideoServiceGrpc.SendingVideoServiceImplBase {
+        @Override
+        public void sendVideo(
+            VideoInfo request,
+            StreamObserver<VideoData> responseObserver
+        ) {
+
+            responseObserver.onCompleted();
+        }
     }
 }
