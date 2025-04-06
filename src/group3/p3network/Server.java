e83aab13a2012428883c6e99b9a7bc19ddf812e7
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 11:43:20 +0800

    Renamed Main to Server

--- src/group3/p3network/Server.java
+++ src/group3/p3network/Server.java
@@ -1,6 +1,6 @@
 package group3.p3network;
 
-public class Main {
+public class Server {
     public static void main(String[] args){
         new VideoSearch(new Directory("server-dir/")).gatherVideos();
     }
