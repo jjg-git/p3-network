fcaa410a4aa872a211f05cf6b4d43c9782a4656a
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 17:27:26 +0800

    Consumer accepts hostname and port as console args

--- src/main/java/group3/p3network/Consumer.java
+++ src/main/java/group3/p3network/Consumer.java
@@ -2,6 +2,18 @@ package group3.p3network;
 
 public class Consumer {
     public static void main(String[] args) {
+        String target = "localhost:50051";
+        if (args.length > 0) {
+            if (args.length == 1 && args[0].equals("--help")) {
+                System.err.println("Syntax: hostname:port");
+                System.err.println("");
+                System.err.println("   hostname  localhost or ip address in " +
+                    "the format of XXX.XXX.XXX.XXX");
+                System.err.println("   port      port number");
+                System.exit(1);
+            }
+            target = args[0];
+        }
 
     }
 }
