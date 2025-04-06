95f0717f5a4e0d48bb388220335543fe62c645e0
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-06 23:29:10 +0800

    Return HashMap instead of ArrayList in VideoSearch

--- src/main/java/group3/p3network/VideoSearch.java
+++ src/main/java/group3/p3network/VideoSearch.java
@@ -1,23 +1,23 @@
 package group3.p3network;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class VideoSearch {
     Directory path;
     public VideoSearch(Directory path) {
         this.path = path;
-        System.out.println("path = " + path.getCurrentDirectory());
+        // System.out.println("path = " + path.getCurrentDirectory());
     }
 
-    public ArrayList<Video> gatherVideos() {
-        ArrayList<Video> videos = new ArrayList<>();
-
-        ArrayList<Path> gatheredVideoFiles = new ArrayList<>();
+    public HashMap<String, Video> gatherVideos() {
+        HashMap<String, Video> videos = new HashMap<>();
 
         for (String result : new File(this.path.getCurrentDirectory()).list()) {
-            videos.add(new Video(path.getCurrentDirectory() + result));
+            videos.put(
+                result,
+                new Video(path.getCurrentDirectory() + result)
+            );
         }
 
         return videos;
