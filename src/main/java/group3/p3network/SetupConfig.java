25a7dcd8152a0b71effac3bbabfb47f674c71974
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-07 10:53:49 +0800

    SetupConfig can now take command line args

--- src/main/java/group3/p3network/SetupConfig.java
+++ src/main/java/group3/p3network/SetupConfig.java
@@ -1,4 +1,25 @@
 package group3.p3network;
 
 public class SetupConfig {
+    public Config setup(String[] args) {
+        Config config = null;
+
+        if (args.length > 0) {
+            if (args[0].equals("--help")) {
+                showHelp();
+                System.exit(1);
+            }
+
+            if (!notConnection(args[1])) {
+                System.exit(1);
+            }
+
+            config = new Config(
+                Integer.parseInt(args[0]),
+                args[1]
+            );
+        }
+
+        return config;
+    }
 }
