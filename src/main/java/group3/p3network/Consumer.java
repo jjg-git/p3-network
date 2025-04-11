b42b480b82a806cda2af945658ae7636ffe24e97
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-11 19:42:28 +0800

    Consumer now uses ConsumerConfig

--- src/main/java/group3/p3network/Consumer.java
+++ src/main/java/group3/p3network/Consumer.java
@@ -2,20 +2,12 @@ package group3.p3network;
 
 import io.grpc.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 public class Consumer {
-    private static String directory = "consumer-dir/";
-    private static String target = "localhost:50051";
-    private final static String defaultDirectory = directory;
-
     private final SendingVideoServiceGrpc.SendingVideoServiceBlockingStub
         blockingStub;
 
@@ -23,15 +15,17 @@ public class Consumer {
         this.blockingStub = SendingVideoServiceGrpc.newBlockingStub(channel);
     }
 
-    public void getFiles(Iterator<VideoInfo> videos) {
+    public void getFiles(ConsumerConfig setting, Iterator<VideoInfo> videos) {
         Iterator<VideoData> streamedData;
-        for (Iterator<VideoInfo> it = videos; it.hasNext(); ) {
-            VideoInfo info = it.next();
+        while (videos.hasNext()) {
+            VideoInfo info = videos.next();
+
             System.out.println("Reading " + info.getFilename() +
                 "(" + info.getFilesize() + ")...");
 
             try {
-                FileOutputStream os = new FileOutputStream(info.getFilename());
+                String filename = setting.output() + info.getFilename();
+                FileOutputStream os = new FileOutputStream(filename);
 
                 streamedData = blockingStub.sendVideo(info);
                 for (int i = 0; streamedData.hasNext(); i++) {
@@ -40,11 +34,7 @@ public class Consumer {
                 }
 
                 os.close();
-            } catch (StatusRuntimeException e) {
-                System.err.println(e.getMessage());
-            } catch (FileNotFoundException e) {
-                System.err.println(e.getMessage());
-            } catch (IOException e) {
+            } catch (StatusRuntimeException | IOException e) {
                 System.err.println(e.getMessage());
             }
         }
@@ -55,77 +45,19 @@ public class Consumer {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length > 0) {
-            handleArgs(args);
-        }
-
-        if (directory.equals(defaultDirectory)) {
-            makeDefaultDirectory();
-        }
-
+        ConsumerConfig setting = new SetupConsumerConfig().setup(args);
 
         ManagedChannel channel = Grpc.newChannelBuilder(
-            target,
+            setting.target(),
             InsecureChannelCredentials.create()
         ).build();
 
         try {
             Consumer consumer = new Consumer(channel);
-            consumer.getFiles(consumer.getVideos());
+            consumer.getFiles(setting, consumer.getVideos());
         } finally {
             channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
         }
     }
 
-    private static void handleArgs(String[] args) {
-        if (args.length == 1 && args[0].equals("--help")) {
-            showHelp();
-            System.exit(1);
-        }
-
-        target = args[0];
-
-        if (args.length > 1) {
-            directory = args[1];
-
-            if (!checkDirectory()) {
-                System.err.println("Cannot find the directory " +
-                    "named \"" + directory + "\".");
-
-                System.exit(1);
-            } else {
-                System.out.println("Found \"" + directory + "\" directory.");
-                // System.exit(0);
-            }
-        }
-    }
-
-    private static void showHelp() {
-        System.err.println("Syntax: hostname:port [output]");
-        System.err.println("");
-        System.err.println("    hostname  localhost or ip address in " +
-            "the format of XXX.XXX.XXX.XXX");
-        System.err.println("    port      port number");
-        System.err.println("    output    name of the directory");
-    }
-
-    private static void makeDefaultDirectory() {
-        Path pathToDefaultDirectory = Path.of(defaultDirectory);
-
-        if (Files.exists(pathToDefaultDirectory)) {
-            return;
-        }
-
-        try {
-            System.err.println("Creating a directory named \""
-                + defaultDirectory + "\" where the videos will be downloaded.");
-            Files.createDirectory(pathToDefaultDirectory);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    private static boolean checkDirectory() {
-        return new File(directory).exists();
-    }
 }
