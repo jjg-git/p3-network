9a593f17cc0fbaad32a55250e1c8366ec22c8e9a
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-12 19:21:04 +0800

    Added test labels into the grid pane

--- src/main/java/group3/p3network/gui/consumer/MainController.java
+++ src/main/java/group3/p3network/gui/consumer/MainController.java
@@ -1,8 +1,23 @@
 package group3.p3network.gui.consumer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.io.File;
 
 public class MainController {
+    @FXML
+    private GridPane videoGrid;
+
+    public void initialize() {
+        Label testLabel = new Label("(0, 0)");
+        Label testLabel2 = new Label("(1, 2)");
+
+        videoGrid.add(testLabel, 0, 0);
+        videoGrid.add(testLabel2, 1, 2);
+    }
+
     @FXML
     public void closeFromMenu() {
         System.out.println("Im closing");
