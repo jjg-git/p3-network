a4467a4ca250193e5ca0023c0d563bf80a4258c5
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-04-12 22:44:51 +0800

    Added the ListView, MediaView and fileList

--- src/main/java/group3/p3network/gui/consumer/MainController.java
+++ src/main/java/group3/p3network/gui/consumer/MainController.java
@@ -3,15 +3,24 @@ package group3.p3network.gui.consumer;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.MediaView;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MainController {
     @FXML
-    private GridPane videoGrid;
+    private ListView videoListView;
+    @FXML
+    private MediaView videoMediaView;
+
+    private ArrayList<File> fileList = new ArrayList<>();
 
     public void initialize() throws IOException {
 
